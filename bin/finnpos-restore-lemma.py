54c723c150f8cff99b8d4659f0e295b8b96c1067
Miikka Silfverberg <miikka.silfverberg@helsinki.fi>
2015-03-04 14:35:56 +0200

    Omorfi lemmatization.

---
+++ bin/finnpos-restore-lemma.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+from sys import stdin
+
+for line in stdin:
+    line = line.strip()
+
+    if line == '':
+        print('')
+    else:
+        wf, feats, lemma, label, ann = line.split('\t')
+
+        if ann.find(' ') != -1:
+            ann = ann[:ann.find(' ')]
+
+        lemma_dict = dict(eval(ann))
+        
+        if label in lemma_dict:
+            lemma = lemma_dict[label]
+
+        print('%s\t%s\t%s\t%s\t%s' % (wf, feats, lemma, label, ann))
