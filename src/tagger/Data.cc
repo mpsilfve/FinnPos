54271c4f35b5dae6d12b647bcc6282932ede35bd
Mikhail Korobov <kmike84@gmail.com>
2015-05-16 20:11:51 +0500

    fixed tests compile error
    Error message:
    
    Word.cc:144:8: fatal error: 'SillyLabelExtractor::set_label_candidates' hides overloaded virtual function [-Woverloaded-virtual]
      void set_label_candidates(const std::string &word_form,
           ^
    ./LabelExtractor.hh:42:16: note: hidden overloaded virtual function 'LabelExtractor::set_label_candidates' declared here: type mismatch at 3rd
          parameter ('float' vs 'unsigned int')
      virtual void set_label_candidates(const std::string &word_form,
                   ^

--- src/tagger/Data.cc
+++ src/tagger/Data.cc
@@ -250,7 +250,7 @@ public:
 
   void set_label_candidates(const std::string &word_form, 
 			    bool use_lexicon,
-			    float mass, 
+			    float count,
 			    LabelVector &target) const
   {
     static_cast<void>(word_form);
