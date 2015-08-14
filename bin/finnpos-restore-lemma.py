b8688879de84b405d8c54add3ca793df54e2f39a
Miikka Silfverberg <miikka.silfverberg@helsinki.fi>
2015-08-14 13:38:10 +0300

    Choose lemma with fewest parts.

--- bin/finnpos-restore-lemma.py
+++ bin/finnpos-restore-lemma.py
@@ -2,6 +2,23 @@
 
 from sys import stdin
 
+def part_count(lemma):
+    return lemma.count('#')
+
+def compile_dict(label_lemma_pairs):
+    res = {}
+
+    for label, lemma in label_lemma_pairs:
+        if label in res:
+            old_lemma = res[label]
+
+            if part_count(old_lemma) > part_count(lemma):
+                res[label] = lemma
+        else:
+            res[label] = lemma
+
+    return res
+
 for line in stdin:
     line = line.strip()
 
@@ -13,9 +30,13 @@ for line in stdin:
         lemmas = ann
         if ann.find(' ') != -1:
             lemmas = ann[:ann.find(' ')]
-            ann = [ann.find(' '):]
-
-        lemma_dict = dict(eval(ann))
+            ann = ann[ann.find(' ') + 1:]
+        else:
+            ann = '_'
+        
+        lemma_dict = {}
+        if lemmas != '_':
+            lemma_dict = compile_dict(eval(lemmas))
         
         if label in lemma_dict:
             lemma = lemma_dict[label]
