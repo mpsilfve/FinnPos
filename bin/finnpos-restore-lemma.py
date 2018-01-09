aa239b69946a37430989dc66815f9662e2e0335f
Miikka Silfverberg <mpsilfve@iki.fi>
2018-01-09 13:12:02 -0700

    Preserve multiple lemmas.

--- bin/finnpos-restore-lemma.py
+++ bin/finnpos-restore-lemma.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 
-from sys import stdin, stdout
+from sys import stdin, stdout, stderr, argv
+
+HASH="<HASH>"
+all_lemmas = 0
 
 def part_count(lemma):
     return lemma.count('#')
@@ -11,13 +14,21 @@ def compile_dict(label_lemma_pairs):
     for label, lemma in label_lemma_pairs:
         if label in res:
             old_lemma = res[label]
-
-            if part_count(old_lemma) > part_count(lemma):
-                res[label] = lemma
+            if old_lemma != lemma:
+                if part_count(old_lemma) > part_count(lemma):
+                    res[label] = lemma
+                elif all_lemmas and part_count(old_lemma) == part_count(lemma):
+                    res[label] = "%s|%s" % (old_lemma.replace("#",HASH),lemma)
         else:
             res[label] = lemma
 
     return res
+
+if "--all-lemmas" in argv:
+    all_lemmas = 1
+elif len(argv) > 1:
+    stderr.write("ERROR: Argument %s unknown.\n" % argv[1])
+    exit(1)
 
 for line in stdin:
     line = line.strip()
@@ -44,4 +55,4 @@ for line in stdin:
             lemma = lemma.lower()
             lemma = lemma.replace('#','')
 
-        print('%s\t%s\t%s\t%s\t%s' % (wf, feats, lemma, label, ann))
+        print('%s\t%s\t%s\t%s\t%s' % (wf, feats, lemma.replace(HASH,"#"), label, ann))
